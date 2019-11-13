8b04aff757ce1d29b6e27304be2553cc803f0fdd
Aart Goossens <aart@goossens.me>
2019-11-13 17:15:02 +0100

    V2 with correct column names and all races

---
+++ constants.py
@@ -0,0 +1,72 @@
+events = [
+    {
+        'name': 'amsterdam_marathon',
+        'year': 2019,
+        'id': 6589788263981649920
+    },
+    {
+        'name': 'amsterdam_marathon',
+        'year': 2018,
+        'id': 6457579627025192192
+    },
+    {
+        'name': 'amsterdam_marathon',
+        'year': 2017,
+        'id': 6324258980229775616
+    },
+    {
+        'name': 'amsterdam_marathon',
+        'year': 2016,
+        'id': 6191605479316130304
+    },
+    {
+        'name': 'dam_tot_damloop',
+        'year': 2019,
+        'id': 6578568117027278336
+    },
+    {
+        'name': 'dam_tot_damloop',
+        'year': 2018,
+        'id': 6447830197266687232
+    },
+    {
+        'name': 'dam_tot_damloop',
+        'year': 2017,
+        'id': 6313393060860395776
+    },
+    {
+        'name': 'dam_tot_damloop',
+        'year': 2016,
+        'id': 6181850543435022336
+    },
+    {
+        'name': 'groet_uit_schoorl_run',
+        'year': 2019,
+        'id': 6498178965006693632
+    },
+    {
+        'name': 'groet_uit_schoorl_run',
+        'year': 2018,
+        'id': 6367295235432645376
+    },
+    {
+        'name': 'groet_uit_schoorl_run',
+        'year': 2017,
+        'id': 6235460488456907776
+    },
+    {
+        'name': 'egmond_halve_marathon',
+        'year': 2019,
+        'id': 6488423419288088576
+    },
+    {
+        'name': 'egmond_halve_marathon',
+        'year': 2018,
+        'id': 6356473683078283264
+    },
+    {
+        'name': 'egmond_halve_marathon',
+        'year': 2017,
+        'id': 6222754489116661504
+    }
+]
