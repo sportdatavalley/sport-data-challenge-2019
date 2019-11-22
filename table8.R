aa214b9ff20d601d2ed18fbbba28fe615043d544
David Kun <david@datascience.ownr.io>
2019-11-22 10:40:07 +0100

    further generalization of the code and getting it ready for the shiny app

--- table8.R
+++ table8.R
@@ -1,50 +1,77 @@
-library(data.table)
 library(ggplot2)
 library(gganimate)
 
 # race results
-results <- read.csv("./data/race_results/results.csv")
+results <- read.csv("./data/race_results/results.csv", stringsAsFactors = FALSE)
 
-# track data
-ams <- read.csv('./data/courses/amsterdam_marathon.csv')
-dam <- read.csv('./data/courses/dam_tot_damloop.csv')
-egmond <- read.csv('./data/courses/egmond_halve_marathon.csv')
-groet <- read.csv('./data/courses/groet_uit_schoorl.csv')
+# track data with names corresponding to the events
+tracks <- list(amsterdam_marathon = read.csv('./data/courses/amsterdam_marathon.csv', stringsAsFactors = FALSE),
+               dam_tot_damloop = read.csv('./data/courses/dam_tot_damloop.csv', stringsAsFactors = FALSE),
+               egmond_halve_marathon = read.csv('./data/courses/egmond_halve_marathon.csv', stringsAsFactors = FALSE),
+               groet_uit_schoorl_run = read.csv('./data/courses/groet_uit_schoorl.csv', stringsAsFactors = FALSE))
 
-# track plots
-plot(ams$latitude~ams$longitude)
-plot(dam$latitude~dam$longitude)
-plot(egmond$latitude~egmond$longitude)
-plot(groet$latitude~groet$longitude)
+# ams <- read.csv('./data/courses/amsterdam_marathon.csv')
+# dam <- read.csv('./data/courses/dam_tot_damloop.csv')
+# egmond <- read.csv('./data/courses/egmond_halve_marathon.csv')
+# groet <- read.csv('./data/courses/groet_uit_schoorl.csv')
+
+# # track plots
+# plot(ams$latitude~ams$longitude)
+# plot(dam$latitude~dam$longitude)
+# plot(egmond$latitude~egmond$longitude)
+# plot(groet$latitude~groet$longitude)
 
 # restrict data to most recent full marathon in Amsterdam
 # this step will become a selector in a shiny app
 # TODO: the variable naming is very specific, should be more generic
+
+event <- "amsterdam_marathon"
+date <- "2019-10-20"
 distance <- 42195
-ams_results_all <- results[results$event_name == "amsterdam_marathon", ]
-ams_results_marathon <- ams_results_all[ams_results_all$distance == distance, ]
-ams_results_marathon_recent <- ams_results_marathon[ams_results_marathon$date == "2019-10-20", ]
-
-# clean the few doubles - note: there are double hashed_names still and that's correct
-ams_results_marathon_recent[c('race', 'id')] <- NULL 
-ams_results_marathon_recent <- unique(ams_results_marathon_recent)
-
 # steps for the interpolation
 distance_tick <- 100
 
-# TODO: this can be different for some events and should be more general
-# steps in the distances actually observed
-x_observed <- c(0, (1:8)*5000, distance)
+# get the track data for the selected event 
+track <- tracks[[event]]
+
+# filter the raw results based on the selection
+raw_results <- results[results$event_name == event & results$distance == distance & results$date == date, ]
+
+# ams_results_all <- results[results$event_name == "amsterdam_marathon", ]
+# ams_results_marathon <- ams_results_all[ams_results_all$distance == distance, ]
+# ams_results_marathon_recent <- ams_results_marathon[ams_results_marathon$date == "2019-10-20", ]
+
+# clean the few doubles - note: there are double hashed_names still and that's correct
+raw_results[c('race', 'id')] <- NULL 
+raw_results <- unique(raw_results)
+
+# ams_results_marathon_recent[c('race', 'id')] <- NULL 
+# ams_results_marathon_recent <- unique(ams_results_marathon_recent)
 
 # TODO: this can be different for some events and should be more general
 # names of the columns that mark the observed times at specific distances
-y_names <- c('split_5k', 'split_10k', 'split_15k', 'split_20k', 'split_25k', 'split_30k', 'split_35k', 'split_40k', 'split_finish')
+observation_names <- c('split_5k', 'split_10k', 'split_15k', 'split_20k', 'split_25k', 'split_30k', 'split_35k', 'split_40k')
+y_names <- c(observation_names, 'split_finish')
+
+# TODO: this can be different for some events and should be more general
+# steps in the distances actually observed
+nr_observations <- length(observation_names)
+
+# remove all non-digit characters from the observation names to get the distances in km and convert it into a number then multiply by a 1000 to get the distance in meters
+x_observed <- as.numeric(unlist(lapply(observation_names, function(name){gsub(pattern = "\\D+", replacement = "", x = name)}))) * 1000
+
+# order the observation points and add start and end points
+x_observed <- c(0, x_observed[order(x_observed)], distance)
 
 # interpolation output points
 xout <- (1 : floor(distance / distance_tick)) * distance_tick
 
 # start time: time between the gun shot and the time of passing the Start point of the race
-ams_results_marathon_recent$start <- ams_results_marathon_recent$gun_time_seconds - ams_results_marathon_recent$chip_time_seconds
+raw_results$start <- raw_results$gun_time_seconds - raw_results$chip_time_seconds
+
+# if this information is missing, set it to 0
+if (is.null(raw_results$start)) {raw_results$start <- 0}
+raw_results$start[is.na(raw_results$start)] <- 0
 
 # Currying of the in-built R interpolation function
 interpolation_function <- function(df){
@@ -52,10 +79,10 @@ interpolation_function <- function(df){
 }
 
 # interpolated run times for each runner
-interpolated_run_time <- t(apply(ams_results_marathon_recent, 1, interpolation_function))
+interpolated_run_time <- t(apply(raw_results, 1, interpolation_function))
 
 # interpolated actual times for each runner (shifted by the start time)
-actual_time <- interpolated_run_time + rep(ams_results_marathon_recent$start)
+actual_time <- interpolated_run_time + rep(raw_results$start)
 
 # function to get the position of a specific runner at a given time
 get_position_at_time <- function(df, time){
@@ -76,11 +103,11 @@ get_congestion_at_time <- function(target_time){
       
       # if position is found, get the coordinates along the track based on distance ran
       if(position > 0){
-        latitude <- unique(ams$latitude[ams$distance == max(ams$distance[ams$distance<position])])
-        longitude <- unique(ams$longitude[ams$distance == max(ams$distance[ams$distance<position])])
+        latitude <- unique(track$latitude[track$distance == max(track$distance[track$distance<position])])
+        longitude <- unique(track$longitude[track$distance == max(track$distance[track$distance<position])])
       } else { # otherwise put them at the starting point
-        latitude <- ams$latitude[1]
-        longitude <- ams$longitude[1]
+        latitude <- track$latitude[1]
+        longitude <- track$longitude[1]
       }
       
       return(c(position, latitude, longitude))
@@ -112,7 +139,7 @@ get_congestion_at_time <- function(target_time){
 
 # times at which congestion is calculated
 time_tick <- 200
-times <- (1 : floor(max(ams_results_marathon_recent$split_finish) / time_tick)) * time_tick
+times <- (1 : floor(max(raw_results$split_finish) / time_tick)) * time_tick
 
 # TODO: make this a parLapply or a for.each to parallel-run and reduce run-time (no pun intended)
 # get the congestion data at each time point for the animation
@@ -120,7 +147,7 @@ congestion_multiple <- do.call(rbind, lapply(times, get_congestion_at_time))
 
 track_points <- do.call(rbind, lapply(times, 
                                       function(target_time){
-                                        return(cbind(ams, data.frame(target_time = target_time)))
+                                        return(cbind(track, data.frame(target_time = target_time)))
                                       }))
 
 # create a plot of the track and add the congestion data 
